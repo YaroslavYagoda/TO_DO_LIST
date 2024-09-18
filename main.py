2b77a2a140fb26a53e238117b8606f43254da7ba
Ярослав <Yagavdv14@yandex.ru>
2024-09-18 13:14:54 +0300

    Готово, без оболочки строго по заданию

---
+++ main.py
@@ -0,0 +1,63 @@
+class ToDoList(object):
+    '''
+    ToDoList - класс для внесения и сопровождения задач.\n
+    Методы:\n
+    add_task(task: string) - добавить задачу в лист исполнения\n
+    complete_task(task: string) - отметить задачу как выполненную\n
+    remove_task(task: string) - удалить задачу из листа исполнения\n
+    list_tasks(task: string) - вывести список всех задач
+    '''
+
+    def __init__(self):
+        self.task_list = {}
+
+    def add_task(self, task):
+        if task not in self.task_list.keys():
+            self.task_list[task] = 'Не выполнена'
+            print(f'Задача "{task}" внесена в лист исполнения\n')
+        else:
+            print(f'Задача "{task}" уже существует\n')
+
+    def complete_task(self, task):
+        if task in self.task_list.keys():
+            self.task_list[task] = 'Выполнена'
+            print(f'Задача "{task}" отмечена как выполненная\n')
+        else:
+            print(f'Задача "{task}" не существует\n')
+
+    def remove_task(self, task):
+        if task in self.task_list.keys():
+            del (self.task_list[task])
+            print(f'Задача "{task}" удалена из списка\n')
+        else:
+            print(f'Задача "{task}" не существует\n')
+
+    def list_tasks(self):
+        print('Список всех задач:\n')
+        for task, status in self.task_list.items():
+            print(f'Задача "{task}"\n\tСтатус - "{status}"\n')
+
+
+task_list = ToDoList()
+
+task_list.add_task('Task1')
+task_list.add_task('Task2')
+task_list.add_task('Task3')
+task_list.add_task('Task1')
+input()
+
+task_list.list_tasks()
+input()
+
+task_list.complete_task('Task1')
+task_list.complete_task('Task2')
+input()
+
+task_list.remove_task('Task2')
+task_list.remove_task('Task2')
+input()
+
+task_list.list_tasks()
+
+
+
