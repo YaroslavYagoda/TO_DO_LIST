0bc9c08bc74ad876e833b02a0f9ab90524b4d426
Ярослав <Yagavdv14@yandex.ru>
2024-09-23 23:37:26 +0300

    Запускать из main, обязательно наличие файлов "test_task.json", "task.json" в том виде как они есть

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from test_task_manager import TestProgram
 
 if __name__ == '__main__':
     test = TestProgram()
-    test.conduct_tests()
-    ext = ''
     tasks_list = TaskManager()
+    ext = ''
     try:
+        test.conduct_tests()
         while ext != 'exit':
             ext = tasks_list.choice_management()
     except Exception as err:
